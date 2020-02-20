79cc33152170d1a40940edf6f61429b5f9d39c04
Jennie Dalgren <jennie.dalgren@gmail.com>
2020-02-20 10:22:56 +0100

    set up most of endpoints in backend. sign up in frontend

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,28 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/auth'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -18,9 +32,59 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+const authenticateUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      accessToken: req.header('Authorization')
+    })
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      res.status(401).json({ loggedOut: true })
+    }
+  } catch (err) {
+    res
+      .status(403)
+      .json({ message: 'access token missing or wrong', errors: err.errors })
+  }
+}
+
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+// Create user  - sign up
+app.post('/users', async (req, res) => {
+  try {
+    const { name, password } = req.body
+    const user = new User({ name, password: bcrypt.hashSync(password) })
+    const saved = await user.save()
+    res.status(201).json(saved)
+  } catch (err) {
+    res.status(400).json({ message: 'could not save user', errors: err.errors })
+  }
+})
+
+// Secure endpoint, user needs to be logged in to access this.
+app.get('/users/:id', authenticateUser)
+app.get('/users/:id', (req, res) => {
+  res.send('YEAH')
+})
+
+// login user
+app.post('/sessions', async (req, res) => {
+  try {
+    const { name, password } = req.body
+    const user = await User.findOne({ name })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({ useId: user._id, accessToken: user.accessToken })
+    } else {
+      res.json({ notFound: true })
+    }
+  } catch (err) {
+    res.status(400).json({ message: 'could not find user', errors: err.errors })
+  }
 })
 
 // Start the server
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const URL = 'http://localhost:8080/users'
 
 export const App = () => {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  // To sign up a user.
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({ name, password }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.log('error:', err))
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <form onSubmit={handleSubmit}>
+        <h1>sign up</h1>
+        <label>
+          name
+          <input
+            required
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+        </label>
+        <label>
+          password
+          <input
+            required
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+        </label>
+        <button type='submit' onClick={handleSubmit}>
+          SIGN UP
+        </button>
+      </form>
     </div>
   )
 }
